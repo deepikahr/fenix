584e172262f72da158f9287ab750d6817d485189
Atish Manala <pro.atish.31@gmail.com>
2021-09-23 18:11:23 +0530

    1) Printer service implemented when user order is confirmed

--- lib/network/printer.dart
+++ lib/network/printer.dart
@@ -2,6 +2,7 @@ import 'package:esc_pos_printer/esc_pos_printer.dart';
 import 'package:esc_pos_utils/esc_pos_utils.dart';
 import 'package:fenix_user/common/constant.dart';
 import 'package:fenix_user/database/db.dart';
+import 'package:fenix_user/models/api_response_models/cart_product/cart_product.dart';
 import 'package:fenix_user/providers/providers.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -12,7 +13,8 @@ class PrinterService {
 
   DB get db => ref.read(dbProvider);
 
-  void _testReceipt(NetworkPrinter printer, {String? waiterName}) {
+  void _testReceipt(NetworkPrinter printer,
+      {String? waiterName, required List<CartProduct> products}) {
     final dateTime = DateTime.now();
     printer.text(
       '${Constants.restaurantName}',
@@ -50,16 +52,16 @@ class PrinterService {
       ),
     );
     printer.hr(ch: '-', linesAfter: 1);
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < products.length; i++) {
       printer.text(
-        'item$i',
+        '${products[i].productName}',
         styles: PosStyles(
           align: PosAlign.left,
         ),
         linesAfter: 1,
       );
       printer.text(
-        '${i + (i * 2)}',
+        '${products[i].variantQuantity}',
         styles: PosStyles(
           align: PosAlign.right,
         ),
@@ -71,7 +73,10 @@ class PrinterService {
     printer.cut();
   }
 
-  Future<bool> printReciept({String? waiterName, String? ip}) async {
+  Future<bool> printReciept(
+      {String? waiterName,
+      String? ip,
+      List<CartProduct> products = const []}) async {
     final _paper = PaperSize.mm80;
     final _profile = await CapabilityProfile.load();
     final _printer = NetworkPrinter(_paper, _profile);
@@ -80,10 +85,7 @@ class PrinterService {
       final PosPrintResult res =
           await _printer.connect(ip ?? ipAddress, port: 9100);
       if (res == PosPrintResult.success) {
-        _testReceipt(
-          _printer,
-          waiterName: waiterName,
-        );
+        _testReceipt(_printer, waiterName: waiterName, products: products);
         _printer.disconnect();
         return true;
       }
